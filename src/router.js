0adef7ae49265a77c42feed933b16801389d097d
Pranav Raut <pranavraut033@gmail.com>
2019-07-04 22:08:00 +0530

    Single Default path
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
-        path: '/',
+        path: '*',
         name: 'home',
         component: Home,
         meta: {
